87769a6c4c48755a1d65e3e24d19c822b32e16a4
GMWolf1988 <gmason.01.19@gmail.com>
2026-01-22 19:37:45 +0000

    Add light theme

---
+++ script.js
@@ -0,0 +1,8 @@
+// Theme toggle functionality
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('light-theme');
+    
+    document.querySelector('.sun-icon').classList.toggle('hidden');
+    document.querySelector('.moon-icon').classList.toggle('hidden');
+}
